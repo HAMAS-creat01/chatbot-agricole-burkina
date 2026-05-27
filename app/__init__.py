d3455fc1c313ad06a5073253c0ae20f93da2bde4
Hama <hamabassibiri@gmail.com>
2026-05-27 02:24:48 +0000

    Fix init avec auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,15 @@ from config import Config
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.secret_key = 'agriculture_burkina_2024'
 
     from app.routes import main
     app.register_blueprint(main)
+
+    from app.auth import auth
+    app.register_blueprint(auth)
+
+    from app.database import init_db
+    init_db()
 
     return app
